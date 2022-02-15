25320c206f2f2bb9da041e1db208f77665e253b0
bitofbeans <benas4sun@gmail.com>
2022-02-14 22:57:51 -0600

    Finished path finding algorithm, added visualization

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ METHOD = "Manhattan"
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 1000
 FPS = 60  # Frames per second
+VISUALIZE = True
 
 # Define colors
 WHITE = (255, 255, 255)
@@ -23,7 +24,7 @@ FONT_ALT = (90, 160, 90)
 
 ROWS = 50
 COLS = 50
-SPACE = int(1)  # Space between tiles
+SPACE = int(0)  # Space between tiles
 # ----- PYGAME --------------------------- #
 pygame.init()  # Start up pygame
 screen = pygame.display.set_mode(
@@ -66,7 +67,7 @@ class GameState:
 
     def __init__(self):
 
-        self.grid = [[" " for col in range(COLS)] for row in range(ROWS)]  # Create grid
+        self.grid = [["." for col in range(COLS)] for row in range(ROWS)]  # Create grid
 
         self.phase = "START"
 
@@ -80,8 +81,9 @@ class GameState:
 
         self.clicked = False
         self.square = 0
+        self.pressed = False
 
-    def render(self):
+    def render(self, visual=False):
         """
         Draws screen and handles inputs
         """
@@ -99,22 +101,46 @@ class GameState:
                 # Logic
                 self.updateTile((x, y))
                 # Visualize colors
-                if self.grid[y][x] == " ":
+                if self.grid[y][x] == ".":
                     color = DGREY
                 elif self.grid[y][x] == "#":
                     color = LGREY
                 elif self.grid[y][x] == "O":
                     color = (3, 252, 161)
-                    start = (x,y)
+                    self.start = (x, y)
                 elif self.grid[y][x] == "X":
                     color = (255, 33, 107)
-                    dest = (x,y)
+                    self.dest = (x, y)
+                elif self.grid[y][x] == "@":
+                    dist = self.calcDist((x, y), self.dest)
+
+                    if visual == True:
+                        if dist < 50:
+                            r = 255
+                            g = round(dist / 50 * 209)
+                            b = 166
+                        else:
+                            r = round((100 - dist) / 50 * 209)
+                            g = 255
+                            b = 166
+                        color = (r, g, b)
+                    else:
+                        color = (70, 130, 255)
                 # Render square
                 pygame.draw.rect(screen, color, self.square, border_radius=2)
 
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_SPACE]:
-            print(self.A_Star(start, dest))
+        if keys[pygame.K_SPACE] and self.pressed == False:
+            self.pressed = True
+
+            path = self.A_Star(self.start, self.dest)  # Returns a path to destination
+
+            if path == "Failure":
+                return
+            self.remove_all_of("@")
+            self.apply_path_to_grid(path, "@")
+        elif not keys[pygame.K_SPACE]:
+            self.pressed = False
 
     def updateTile(self, pos):
 
@@ -122,10 +148,11 @@ class GameState:
         Usage: squareLogic((x,y), square, mouseClicked, mousePos)
         Handles inputs to each square
 
-            " " = Empty space
+            "." = Empty space
             "#" = Wall/Barrier
             "O" = Start
             "X" = Destination
+            "@" = Path
         """
         """
                 If a tile is clicked, check if it is open.
@@ -138,7 +165,7 @@ class GameState:
         if (
             self.square.collidepoint(self.mousePos) and self.mouseClicked[0]
         ):  # If clicked
-            if self.grid[y][x] == " ":  # And empty space
+            if self.grid[y][x] == ".":  # And empty space
 
                 if not self.clicked:
                     if self.phase == "START":
@@ -184,7 +211,7 @@ class GameState:
     def calcCost(self, start, end):
         sx, sy = start
         ex, ey = end
-        if (sx is ex) or (sy is ey):
+        if (sx == ex) or (sy == ey):
             return 1.0
         else:
             return 1.41
@@ -192,7 +219,7 @@ class GameState:
     def findNeighbors(self, node):
         def tryAdd(nx, ny):
             try:
-                if self.grid[ny][nx]  != "#":  # If it is not a wall,
+                if self.grid[ny][nx] != "#":  # If it is not a wall,
                     neighbors.append((nx, ny))  # add to neighbors list
             except:
                 pass
@@ -202,16 +229,16 @@ class GameState:
         if not x == 0:  # Account for left edge
             tryAdd(x - 1, y)  # Left
             # DIAGONALS
-            if not y ==  0:  # Account for top edge
+            if not y == 0:  # Account for top edge
                 tryAdd(x - 1, y - 1)  # Top
             if not y == 49:  # Account for bottom edge
                 tryAdd(x - 1, y + 1)  # Bottom
-        if not x ==  49:  # Account for right edge
+        if not x == 49:  # Account for right edge
             tryAdd(x + 1, y)  # Right
             # DIAGONALS
-            if not y ==  0:  # Account for top edge
+            if not y == 0:  # Account for top edge
                 tryAdd(x + 1, y - 1)  # Top
-            if not y ==  49:  # Account for bottom edge
+            if not y == 49:  # Account for bottom edge
                 tryAdd(x + 1, y + 1)  # Bottom
         if not y == 0:  # Account for top edge
             tryAdd(x, y - 1)  # Top
@@ -233,6 +260,12 @@ class GameState:
         return totalPath
 
     def A_Star(self, start, dest):
+        """
+        The A* pathfinding algorithm.
+
+        Takes a starting point and ending point and calculates the fastest path between them
+        """
+
         ## calcDist() is used as h() ##
         ## Inspired by https://en.wikipedia.org/wiki/A*_search_algorithm
         # Tuples unpacked to coordinates
@@ -258,13 +291,18 @@ class GameState:
         fScore = [[99999 for col in range(COLS)] for row in range(ROWS)]
         fScore[startY][startX] = self.calcDist(start, dest)
 
-        while openSet: # While not empty
+        # To record the loop cycles
+        cycles = 0
+
+        while openSet:  # While not empty
             # Find the node in the openSet with the lowest f value
             lowest = 99999
             i = 0  # Start at the end of the list
             for node in openSet:  # Reversed means it reversed the set
-                if openSetFScores[i] < lowest:  # If it is the lowest value so far,
+                temp_fScore = openSetFScores[i]
+                if temp_fScore < lowest:  # If it is the lowest value so far,
                     current = node  # Set the current node to it
+                    lowest = temp_fScore
                 i += 1
 
             if current == dest:
@@ -274,29 +312,72 @@ class GameState:
                 )  # Recreate path to destination
 
             currentIDX = openSet.index(current)
-            openSet.remove(current)
-            openSetFScores.pop(currentIDX)
+            openSet.remove(current)  # Remove current from open set
+            openSetFScores.pop(currentIDX)  # And remove its fScore value
             # Go through all valid neighbors
             for neighbor in self.findNeighbors(current):
-                
+
                 currentX, currentY = current
                 neighborX, neighborY = neighbor
-                if self.grid[neighborY][neighborX] == "#": continue
+                if self.grid[neighborY][neighborX] == "#":
+                    continue  # if it is a wall, stop
                 # Trial_gScore is the distance from the start to the neighbor, through current node
                 trial_gScore = gScore[currentY][currentX] + self.calcCost(
                     current, neighbor
                 )
                 if trial_gScore < gScore[neighborY][neighborX]:
                     # This is the best path so far to the neighbor
+                    # Record values
                     cameFrom[neighbor] = current
                     gScore[neighborY][neighborX] = trial_gScore
                     trial_fScore = trial_gScore + self.calcDist(neighbor, dest)
                     fScore[neighborY][neighborX] = trial_fScore
 
                     if not neighbor in openSet:
-                        openSet.append(neighbor)
-                        openSetFScores.append(trial_fScore)
+                        openSet.append(neighbor)  # Add to set
+                        openSetFScores.append(trial_fScore)  # and add value
+
+            cycles += 1
+            if cycles > 1500:
+                raise SystemError("Pathfinding took too long (no path?)")
+
+            # ___----*** VISUALIZE ***----___ #
+            if VISUALIZE == False:
+                continue
+
+            self.apply_path_to_grid(self.reconstruct_path(cameFrom, current), "@")
+
+            self.render(visual=True)
+
+            # --- PyGame Display --- #
+            pygame.display.update()
+            clock.tick(FPS / 2)  # Set frame rate
+
         return "Failure"
+
+    def apply_path_to_grid(self, path, value):
+        """
+        Takes in a list of nodes and applies changes to the grid according to the input.
+
+        Usage: apply_path_to_grid(path, "@")
+        """
+        for node in path:
+            x, y = node
+            if self.grid[y][x] != "X" and self.grid[y][x] != "O":
+                self.grid[y][x] = value
+
+    def remove_all_of(self, value):
+        """
+        Removes all instances of a value in the grid.
+
+        Usage: remove_all_of("@")
+        """
+        rowCount = 0
+        for row in self.grid:
+            self.grid[rowCount] = [
+                x if x != value else "." for x in self.grid[rowCount]
+            ]  # removes all of an instance
+            rowCount += 1
 
 
 # ----- INITIALIZE ------------------------ #
@@ -317,53 +398,3 @@ while run:
 
 # ----- END ------------------------ #
 pygame.quit()  # Close
-
-"""function reconstruct_path(cameFrom, current)
-    total_path := {current}
-    while current in cameFrom.Keys:
-        current := cameFrom[current]
-        total_path.prepend(current)
-    return total_path
-
-// A* finds a path from start to goal.
-// h is the heuristic function. h(n) estimates the cost to reach goal from node n.
-function A_Star(start, goal, h)
-    // The set of discovered nodes that may need to be (re-)expanded.
-    // Initially, only the start node is known.
-    // This is usually implemented as a min-heap or priority queue rather than a hash-set.
-    openSet := {start}
-
-    // For node n, cameFrom[n] is the node immediately preceding it on the cheapest path from start
-    // to n currently known.
-    cameFrom := an empty map
-
-    // For node n, gScore[n] is the cost of the cheapest path from start to n currently known.
-    gScore := map with default value of Infinity
-    gScore[start] := 0
-
-    // For node n, fScore[n] := gScore[n] + h(n). fScore[n] represents our current best guess as to
-    // how short a path from start to finish can be if it goes through n.
-    fScore := map with default value of Infinity
-    fScore[start] := h(start)
-
-    while openSet is not empty
-        // This operation can occur in O(1) time if openSet is a min-heap or a priority queue
-        current := the node in openSet having the lowest fScore[] value
-        if current = goal
-            return reconstruct_path(cameFrom, current)
-
-        openSet.Remove(current)
-        for each neighbor of current
-            // d(current,neighbor) is the weight of the edge from current to neighbor
-            // tentative_gScore is the distance from start to the neighbor through current
-            tentative_gScore := gScore[current] + d(current, neighbor)
-            if tentative_gScore < gScore[neighbor]
-                // This path to neighbor is better than any previous one. Record it!
-                cameFrom[neighbor] := current
-                gScore[neighbor] := tentative_gScore
-                fScore[neighbor] := tentative_gScore + h(neighbor)
-                if neighbor not in openSet
-                    openSet.add(neighbor)
-
-    // Open set is empty but goal was never reached
-    return failure"""
